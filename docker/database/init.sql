6f0053f7a49f28f8a55e98945149de24adc86435
LeomaxDesign <gurov_w@mail.ru>
2020-09-27 19:31:51 +0300

    updated db dump

--- docker/database/init.sql
+++ docker/database/init.sql
@@ -5,8 +5,6 @@
 -- Dumped from database version 12.3
 -- Dumped by pg_dump version 12.3
 
--- Started on 2020-09-26 19:29:31
-
 SET statement_timeout = 0;
 SET lock_timeout = 0;
 SET idle_in_transaction_session_timeout = 0;
@@ -23,11 +21,10 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
--- TOC entry 203 (class 1259 OID 33030)
--- Name: feed; Type: TABLE; Schema: public; Owner: postgres
+-- Name: news_feeds; Type: TABLE; Schema: public; Owner: postgres
 --
 
-CREATE TABLE public.feed (
+CREATE TABLE public.news_feeds (
     id bigint NOT NULL,
     url character varying NOT NULL,
     title character varying NOT NULL,
@@ -43,28 +40,23 @@ CREATE TABLE public.feed (
 );
 
 
-ALTER TABLE public.feed OWNER TO postgres;
+ALTER TABLE public.news_feeds OWNER TO postgres;
 
 --
--- TOC entry 2838 (class 0 OID 0)
--- Dependencies: 203
--- Name: COLUMN feed.frequency; Type: COMMENT; Schema: public; Owner: postgres
+-- Name: COLUMN news_feeds.frequency; Type: COMMENT; Schema: public; Owner: postgres
 --
 
-COMMENT ON COLUMN public.feed.frequency IS 'in seconds';
+COMMENT ON COLUMN public.news_feeds.frequency IS 'in seconds';
 
 
 --
--- TOC entry 2839 (class 0 OID 0)
--- Dependencies: 203
--- Name: COLUMN feed.parse_count; Type: COMMENT; Schema: public; Owner: postgres
+-- Name: COLUMN news_feeds.parse_count; Type: COMMENT; Schema: public; Owner: postgres
 --
 
-COMMENT ON COLUMN public.feed.parse_count IS 'news parse count';
+COMMENT ON COLUMN public.news_feeds.parse_count IS 'news parse count';
 
 
 --
--- TOC entry 202 (class 1259 OID 33028)
 -- Name: feed_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -79,20 +71,17 @@ CREATE SEQUENCE public.feed_id_seq
 ALTER TABLE public.feed_id_seq OWNER TO postgres;
 
 --
--- TOC entry 2840 (class 0 OID 0)
--- Dependencies: 202
 -- Name: feed_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
-ALTER SEQUENCE public.feed_id_seq OWNED BY public.feed.id;
+ALTER SEQUENCE public.feed_id_seq OWNED BY public.news_feeds.id;
 
 
 --
--- TOC entry 205 (class 1259 OID 33042)
--- Name: feed_news; Type: TABLE; Schema: public; Owner: postgres
+-- Name: news; Type: TABLE; Schema: public; Owner: postgres
 --
 
-CREATE TABLE public.feed_news (
+CREATE TABLE public.news (
     id bigint NOT NULL,
     feed_id bigint,
     title character varying,
@@ -104,10 +93,9 @@ CREATE TABLE public.feed_news (
 );
 
 
-ALTER TABLE public.feed_news OWNER TO postgres;
+ALTER TABLE public.news OWNER TO postgres;
 
 --
--- TOC entry 204 (class 1259 OID 33040)
 -- Name: feed_news_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -122,67 +110,57 @@ CREATE SEQUENCE public.feed_news_id_seq
 ALTER TABLE public.feed_news_id_seq OWNER TO postgres;
 
 --
--- TOC entry 2841 (class 0 OID 0)
--- Dependencies: 204
 -- Name: feed_news_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
-ALTER SEQUENCE public.feed_news_id_seq OWNED BY public.feed_news.id;
+ALTER SEQUENCE public.feed_news_id_seq OWNED BY public.news.id;
 
 
 --
--- TOC entry 2695 (class 2604 OID 33033)
--- Name: feed id; Type: DEFAULT; Schema: public; Owner: postgres
+-- Name: news id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY public.feed ALTER COLUMN id SET DEFAULT nextval('public.feed_id_seq'::regclass);
-
-
---
--- TOC entry 2698 (class 2604 OID 33045)
--- Name: feed_news id; Type: DEFAULT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.feed_news ALTER COLUMN id SET DEFAULT nextval('public.feed_news_id_seq'::regclass);
+ALTER TABLE ONLY public.news ALTER COLUMN id SET DEFAULT nextval('public.feed_news_id_seq'::regclass);
 
 
 --
--- TOC entry 2704 (class 2606 OID 33050)
--- Name: feed_news feed_news_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+-- Name: news_feeds id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY public.feed_news
+ALTER TABLE ONLY public.news_feeds ALTER COLUMN id SET DEFAULT nextval('public.feed_id_seq'::regclass);
+
+
+--
+-- Name: news feed_news_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.news
     ADD CONSTRAINT feed_news_pkey PRIMARY KEY (id);
 
 
 --
--- TOC entry 2706 (class 2606 OID 33064)
--- Name: feed_news feed_news_title_description_link_key; Type: CONSTRAINT; Schema: public; Owner: postgres
+-- Name: news feed_news_title_description_link_key; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY public.feed_news
+ALTER TABLE ONLY public.news
     ADD CONSTRAINT feed_news_title_description_link_key UNIQUE (title) INCLUDE (description, link);
 
 
 --
--- TOC entry 2700 (class 2606 OID 33039)
--- Name: feed feed_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+-- Name: news_feeds feed_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY public.feed
+ALTER TABLE ONLY public.news_feeds
     ADD CONSTRAINT feed_pkey PRIMARY KEY (id);
 
 
 --
--- TOC entry 2702 (class 2606 OID 33060)
--- Name: feed feed_url_key; Type: CONSTRAINT; Schema: public; Owner: postgres
+-- Name: news_feeds feed_url_key; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY public.feed
+ALTER TABLE ONLY public.news_feeds
     ADD CONSTRAINT feed_url_key UNIQUE (url);
 
-
--- Completed on 2020-09-26 19:29:31
 
 --
 -- PostgreSQL database dump complete
